4dd29205e5fab2ab365c3ccf52ccac3597033725
TSOlami <tijani.saheed@yahoo.com>
2023-08-21 18:38:00 +0100

    add documentation

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,6 +6,9 @@ A function named index_range that takes two integer arguments
 from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    A start index and an end index corresponding to the range of indexes
+    """
     start_index = (page - 1) * page_size
     end_index = start_index * page_size
     return (start_index, end_index)
