4d0fc29d3cb79a74429d22b21516adc7e343293b
TSOlami <tijani.saheed@yahoo.com>
2023-08-21 18:31:56 +0100

    add types

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,7 +3,7 @@
 A function named index_range that takes two integer arguments
 
 """
+from typing import Tuple
 
-
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
     pass
