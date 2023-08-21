fa256f53350afca526e9ce07cfe4b30157443285
TSOlami <tijani.saheed@yahoo.com>
2023-08-21 19:10:33 +0100

    add code from previous task

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,47 @@
+import csv
+from typing import Tuple, List
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """
+    A start index and an end index corresponding to the range of indexes
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """
+        Initializes a new Server instance.
+        """
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Use assert to verify that both arguments are integers greater than 0
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        # Use index_range to find the correct indexes to paginate
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
