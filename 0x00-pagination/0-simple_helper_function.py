8fe18101c5652afdb022cc946b0866a111b583ee
TSOlami <tijani.saheed@yahoo.com>
2023-08-21 18:39:49 +0100

    fix pycodestyle indentation error

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,9 +1,10 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 """
 A function named index_range that takes two integer arguments
 
 """
 from typing import Tuple
+
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """
