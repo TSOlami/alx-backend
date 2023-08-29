c7b0e931b08911dd0a6daa5ea28baebd0b987a96
TSOlami <tijani.saheed@yahoo.com>
2023-08-29 18:41:03 +0100

    Remove unused variables

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-"""Parametrize templates
+"""Setup a basic Flask app in 1-app.py.
+Create a single / route and an index.html template
 """
-from flask import Flask
-from flask_babel import Babel
-from flask import render_template, request
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
 
 
-class Config:
-    """Flask Babel configuration.
-    """
+class Config(object):
+    """Class to configure available languages in our app"""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
@@ -22,17 +22,17 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale() -> str:
-    """ _ or gettext function to parametrize your templates
+    """Determine the best match with our supported languages.
     """
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def get_index() -> str:
-    """The home/index page.
-    """
-    return render_template('3-index.html')
+def index() -> str:
+    """Function that returns an index page"""
+    return render_template(
+        "3-index.html")
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
